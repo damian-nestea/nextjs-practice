feb2482a260c3163d5742cc1258c0189961c37f1
damian-nestea <damian@hotmail.cz>
2023-03-31 18:53:39 -0300

    feature criação de getStaticProps para passar a informação do post na API para o elemento que será renderizado quando for clicado

--- my-first-nextjs-app/src/pages/blog/[id].js
+++ my-first-nextjs-app/src/pages/blog/[id].js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const BlogItem = ({ post }) => {
   return (
-    <div>
-        <h1>Blog</h1>
-        <h2>{post.titulo}</h2>
-        <p>{post.body}</p>
+    <div className='flex flex-col gap-8 py-20 px-10'>
+        <h1 className=' text-xl font-bold text-center'>Blog</h1>
+        <h2 className=' text-lg font-bold text-center'>{post.title}</h2>
+        <p className=' px-64 text-center'>{post.body}</p>
     </div>
   )
 }
@@ -17,13 +17,19 @@ export const getStaticPaths = async () => {
     const posts = await res.json();
 
     const paths = posts.map(post => ({
-        params: {
-            id:post.id.toString()
-        }
+        params: { id: post.id.toString()}
     }));
     return { paths , fallback: false}
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps = async (context) => {
+    const { id } = context.params;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const post = await res.json(); 
 
+    return{
+        props: {
+            post
+        }
+    }
 }
