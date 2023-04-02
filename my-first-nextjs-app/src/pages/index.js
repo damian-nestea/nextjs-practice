11a19809012afae88c842738cda40a45f0739af8
damian-nestea <damian@hotmail.cz>
2023-04-02 20:27:14 -0300

    feature adição do componente link para uso no site

--- my-first-nextjs-app/src/pages/index.js
+++ my-first-nextjs-app/src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Script from 'next/script'
+import Link from 'next/link'
 
 export default function Home() {
 
@@ -17,20 +18,20 @@ export default function Home() {
         {/* Usando variável de ambiente na frase abaixo */}
         Eu sou o {process.env.NEXT_PUBLIC_NOME}
         <ul className='flex flex-col gap-4'>
-          <li><a href="/damian">Página de Damián</a></li>
-          <li><a href="/brenda">Página de Brenda</a></li>
-          <li><a href="/valentina">Página de Valentina</a></li>
-          <li><a href="/isis">Página de Isis</a></li>
-          <li><a href="/lana">Página de Lana</a></li>
-          <li><a href="/tyson">Página de Tyson</a></li>
+          <li><Link href="/damian">Página de Damián</Link></li>
+          <li><Link href="/brenda">Página de Brenda</Link></li>
+          <li><Link href="/valentina">Página de Valentina</Link></li>
+          <li><Link href="/isis">Página de Isis</Link></li>
+          <li><Link href="/lana">Página de Lana</Link></li>
+          <li><Link href="/tyson">Página de Tyson</Link></li>
         </ul>
         {/* <Script strategy='afterInteractive'>
           {`window.alert('Carreguei')`}
         </Script> */}
       <footer className=' mt-10 flex flex-col gap-5'>
-        <a href='/blog'>EXERCÍCIO SSG - BLOG</a>
-        <a href='/album'>EXERCÍCIO CSR - ALBUM</a>
-        <a href='/todo'>EXERCÍCIO SSR - LISTA DE TAREFAS</a>
+        <Link href='/blog' legacyBehavior><a target='_blank'>EXERCÍCIO SSG - BLOG</a></Link>
+        <Link href='/album'>EXERCÍCIO CSR - ALBUM</Link>
+        <Link href='/todo'>EXERCÍCIO SSR - LISTA DE TAREFAS</Link>
       </footer>
       </main>
     </>
