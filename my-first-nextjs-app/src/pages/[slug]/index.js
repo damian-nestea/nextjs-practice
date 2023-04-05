fdb70ea1d456538669b74bf69b527439b4b1371b
damian-nestea <damian@hotmail.cz>
2023-04-05 12:56:46 -0300

    feature

--- my-first-nextjs-app/src/pages/[slug]/index.js
+++ my-first-nextjs-app/src/pages/[slug]/index.js
@@ -32,6 +32,7 @@ const SobreItem = () => {
                 <h1>Página de <span className='font-bold underline uppercase text-orange-500'>{slug}</span></h1>
                 <p>Pathname : {router.pathname}</p>
                 <p>IsFallback : {router.isFallback.toString()}</p>
+                <p>Query : {slug.query}</p>
             </main>
             <footer className='bg-gray-400 min-h-1/5 flex px-32 py-2'>
                 <p className='flex items-center'><button onClick={() => {router.push('../')}} className=' bg-gradient-to-tr from-blue-700 to-gray-700 p-2 m-1 text-neutral-400 rounded hover:ring-red-400 hover:ring-4'>VOLTAR</button></p>
