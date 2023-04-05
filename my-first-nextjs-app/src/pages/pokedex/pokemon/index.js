8f520482abe7212d9e810edfa738fd9fd4695854
damian-nestea <damian@hotmail.cz>
2023-04-04 22:20:08 -0300

    fix correção do slug para aparecer o nome do pokemon assim que clicado

--- my-first-nextjs-app/src/pages/pokedex/pokemon/index.js
+++ my-first-nextjs-app/src/pages/pokedex/pokemon/index.js
@@ -1,21 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const loadPokemon = async (url) => {
+    let response = await fetch(url);
+    let json = await  response.json();
+    console.log(json);
+}
 
 const Pokemon = ({ pokemon }) => {
+    console.log(pokemon)
   return (
     <li>{pokemon.name} : {pokemon.url}</li>
   )
 }
 
 export default Pokemon;
-
-export const getServerSideProps = async (context) => {
-    console.log(context)
-    const res = await fetch(pokemon.url);
-    const pokemonList = await res.json();
-    console.log(pokemonList)
-    return {
-        props: {
-            pokedex: pokemonList.results
-        }
-    }
-}
