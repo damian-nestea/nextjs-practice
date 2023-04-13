4d43766032e31e942ac6f97f1efa4fdffc2b5f3a
damian-nestea <damian@hotmail.cz>
2023-04-12 21:06:18 -0300

    feature consulta na API do HP puxando os personagens e renderizando o nome dos personagens na página

--- my-first-nextjs-app/src/pages/harryPotter/index.js
+++ my-first-nextjs-app/src/pages/harryPotter/index.js
@@ -1,10 +1,23 @@
-const HarryPotterCharacters = () => {
+const HarryPotterCharacters = ({ characters }) => {
+    console.log(characters)
+    const personagens = characters.map((personagem) => {
+        return <p>{personagem.name}</p>
+    })
     return(
         <div>
-            ...        
+            {personagens}
         </div>
     )
 }
 
 export default HarryPotterCharacters;
 
+export const getServerSideProps = async () => {
+    const res = await fetch("https://hp-api.onrender.com/api/characters");
+    const hpCharacters = await res.json();
+    return {
+        props: {
+            characters: hpCharacters
+        }
+    }
+}
