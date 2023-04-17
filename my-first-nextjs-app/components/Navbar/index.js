def39016086fd5235f873ee53b77486b992f4f4a
damian-nestea <damian@hotmail.cz>
2023-04-17 18:40:54 -0300

    feature adição de estilização caso a path seja filha para aplicar estilização com criação de função verificaPath

--- my-first-nextjs-app/components/Navbar/index.js
+++ my-first-nextjs-app/components/Navbar/index.js
@@ -6,10 +6,16 @@ import { useRouter } from 'next/router'
 const Navbar = () => {
   const router = useRouter();
 
+  const verifyPath = (linkPath) => {
+    if(linkPath ==='/' && router.pathname !=='/') return false;
+    if(router.pathname.indexOf(linkPath) === 0) return true;
+    return false
+  }
+
   return (
     <ul className={`flex gap-8 list-none uppercase font-bold`}>
       {navigationLinks.map((link,index) => (
-        <li className={(link.path === router.pathname ? 'text-red-600' : ' text-blue-800')} key={index}>
+        <li className={( verifyPath(link.path) ? 'text-red-600' : ' text-blue-800')} key={index}>
           <Link href={link.path}>{link.label}</Link>
         </li>
       ))}
