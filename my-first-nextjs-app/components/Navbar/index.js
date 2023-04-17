80fb96c6c94993a2ca6c6a6eb6c04971291d718d
damian-nestea <damian@hotmail.cz>
2023-04-17 18:19:34 -0300

    feature adição de estilização por condição com tailwindcss usando o hook router

--- my-first-nextjs-app/components/Navbar/index.js
+++ my-first-nextjs-app/components/Navbar/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { navigationLinks } from '../../utils/data'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
-    <ul className=' flex gap-8 list-none uppercase'>
+    <ul className={`flex gap-8 list-none uppercase font-bold`}>
       {navigationLinks.map((link,index) => (
-        <li className=' hover:underline' key={index}>
+        <li className={(link.path === router.pathname ? 'text-red-600' : ' text-blue-800')} key={index}>
           <Link href={link.path}>{link.label}</Link>
         </li>
       ))}
