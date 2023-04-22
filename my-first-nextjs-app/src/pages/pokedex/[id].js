46112dd10c83b0e4c68717633dac8b5cc09ece90
damian-nestea <damian@hotmail.cz>
2023-04-22 17:04:43 -0300

    feature adição de metatags para SEO na página dinâmica de pokemons

--- my-first-nextjs-app/src/pages/pokedex/[id].js
+++ my-first-nextjs-app/src/pages/pokedex/[id].js
@@ -1,40 +1,68 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from "next/head";
+import React from "react";
 
 const PokemonPage = ({ pokemon }) => {
   return (
-    <article className=' flex flex-col mx-auto my-8 gap-3 w-48'>
+    <article className=" flex flex-col mx-auto my-8 gap-3 w-48">
       <Head>
-        <title>Pokemon: {pokemon.name}</title>
-      </Head>
-      <h1 className=' text-2xl font-bold first-letter:uppercase text-center'>{pokemon.name}</h1>
-      <p className=' text-center'>Peso: {pokemon.height}kg.</p>
-      <img className=' min-w-full' src={pokemon.sprites.back_default} />
-    </article>
-  )
-}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
 
-export default PokemonPage
+        <title>{pokemon.name}</title>
+        <meta name="title" content={pokemon.name} />
+        <meta name="description" content={`Página com o pokemon ${pokemon.name}`} />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content={`http://localhost:3000/pokedex/${pokemon.id}`}
+        />
+        <meta property="og:title" content={pokemon.name} />
+        <meta property="og:description" content={`Página com o pokemon ${pokemon.name}`} />
+        <meta property="og:image" content="http://localhost:3000/logo.png" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`http://localhost:3000/pokedex/${pokemon.id}`}
+        />
+        <meta property="twitter:title" content={pokemon.name} />
+        <meta property="twitter:description" content={`Página com o pokemon ${pokemon.name}`} />
+        <meta
+          property="twitter:image"
+          content="http://localhost:3000/logo.png"
+        ></meta>
+      </Head>
+      <h1 className=" text-2xl font-bold first-letter:uppercase text-center">
+        {pokemon.name}
+      </h1>
+      <p className=" text-center">Peso: {pokemon.height}kg.</p>
+      <img className=" min-w-full" src={pokemon.sprites.back_default} />
+    </article>
+  );
+};
+
+export default PokemonPage;
 
 export const getStaticPaths = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon");
   const pokemonList = await res.json();
 
-    const paths = pokemonList.results.map((pokemon,index) => ({
-        params: { id: (index + 1).toString()}
-    }));
-    return { paths , fallback: false}
-}
+  const paths = pokemonList.results.map((pokemon, index) => ({
+    params: { id: (index + 1).toString() },
+  }));
+  return { paths, fallback: false };
+};
 
 export const getStaticProps = async (context) => {
   const { id } = context.params;
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-  const pokemon = await res.json(); 
+  const pokemon = await res.json();
 
-  console.log(pokemon)
-  return{
-      props: {
-          pokemon
-      }
-  }
-}
+  console.log(pokemon);
+  return {
+    props: {
+      pokemon,
+    },
+  };
+};
